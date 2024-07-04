f65b7564ccd7dab5c2e9744457c345e6cad8eeed
rodgerswisdom <trulyhawona@gmail.com>
2024-07-05 00:03:19 +0300

    feat: calculator

--- calculator.php
+++ calculator.php
@@ -1,1 +1,19 @@
 <?php
+
+ class Calculator{
+    public $num1;
+    public $num2;
+
+    public function __construct($num1, $num2){
+        $this->num1 = $num1;
+        $this->num2 = $num2;
+    }
+
+    public function plus($num1, $num2){
+        return $this->plus($num1, $num2);
+    }
+}
+
+$myCalculator = new Calculator(50,60);
+
+echo $myCalculator;
