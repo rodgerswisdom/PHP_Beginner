b48c0cb061ce3e405f70e9385b6eaad56aa45260
rodgerswisdom <trulyhawona@gmail.com>
2025-02-07 01:46:45 +0300

    Automated commit

---
+++ form_handler.php
@@ -0,0 +1,12 @@
+<?php
+
+function get_form($f_name, $l_name, $email, $phone)
+{
+    if(isset($_POST['submit'])){
+        $f_name = $_POST['f_name'];
+        $l_name = $_POST['l_name'];
+        $email = $_POST['email'];
+        $phone = $_ POST['phone'];
+    }
+}
+?>
